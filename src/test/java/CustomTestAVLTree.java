5d0e4e32f2f16f618f003f8c0eb13053b5a23ab7
2093976-Vikram-Pillarisetty <103394449+2093976-Vikram-Pillarisetty@users.noreply.github.com>
2022-04-17 00:46:30 -0500

    Update CustomTestAVLTree.java

--- src/test/java/CustomTestAVLTree.java
+++ src/test/java/CustomTestAVLTree.java
@@ -1,21 +1,25 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class CustomTestAVLTree {
 
   @Test
   public void test01()  throws Throwable  {
-    AVLTree aVLTree0 = new AVLTree();
-    aVLTree0.insert(0);
-    aVLTree0.height();
-    AVLTree.Node aVLTree_Node0 = aVLTree0.find(0);
-    aVLTree0.delete(914);
-    aVLTree_Node0.left = aVLTree0.getRoot();
-    aVLTree0.find(0);
-    aVLTree0.find(0);
-    int int0 = aVLTree0.getBalance(aVLTree_Node0);
-    aVLTree0.insert(int0);
-    assertEquals(4, int0);
+    try {
+      AVLTree aVLTree0 = new AVLTree();
+      aVLTree0.insert(0);
+      aVLTree0.height();
+      AVLTree.Node aVLTree_Node0 = aVLTree0.find(0);
+      aVLTree0.delete(914);
+      aVLTree_Node0.left = aVLTree0.getRoot();
+      aVLTree0.find(0);
+      aVLTree0.find(0);
+      int int0 = aVLTree0.getBalance(aVLTree_Node0);
+      aVLTree0.insert(int0);
+    }catch(StackOverflowError error) {
+      fail("Stack is overflowed because of no right null check");
+    }
   }
 }
